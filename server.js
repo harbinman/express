8645480783e42df7d357036c4b10a3ae45867df7
harbinman <harbinman@163.com>
2023-09-17 14:32:46 +0800

    new

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
-app.keepAliveTimeout = 65000; // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
-app.headersTimeout = 66000;
+const port = 3000;
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
